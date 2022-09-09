7c9ad387799677d2376c5b8858dc739473cac11c
Fabio Schitini Alves de Oliveira <fabioschitini1@hotmail.com>
2022-09-09 19:02:02 -0300

    Create alunonaEscola.c

---
+++ alunonaEscola.c
@@ -0,0 +1,13 @@
+typedef struct alunos{
+    char matricula[20];
+    char Nome[40];
+    char sexo[1];
+    char nascimento[15];
+    char cpf[20];
+} Aluno;
+
+Aluno alunos[999];
+for(int i=0;i>0;){
+    printf("Coloque o nome do Alunos");
+    fgets(alunos);
+}
