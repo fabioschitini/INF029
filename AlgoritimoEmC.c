6d72297e7317b87259ba22a12a0f293a482bb38b
Fabio Schitini Alves de Oliveira <fabioschitini1@hotmail.com>
2022-09-08 21:33:20 -0300

    Create AlgoritimoEmC.c

---
+++ AlgoritimoEmC.c
@@ -0,0 +1,40 @@
+//Binario
+
+int A[n];
+inicio=0;
+fim=n-1;
+while(ini<=fim){ logN na base 2+1 +3*(log de n na bse 2+1)=4 logn..
+    meio=(fim+inicio)/2;
+    if(A[meio==v]){
+        achaste!
+    }
+    else if(A[meio<v]){
+        inicio=meio+1;
+    }
+    else{
+        fim=meio-1;
+    }
+}
+logN na base 2+1
+  
+  //Selecao
+  
+int main() {
+  //Selecao
+  
+  int vet[5]={4,8,3,5,1};
+for(int i=0;i<n-1;i++){ // n-1
+        menor=i; //n-1
+    for(int j=i+1;j<n-1;j++){  //soma de pa  n(n-1)/2 j=1 a n-1,=1
+        if(vet[j]>vet[menor]){
+            menor=j;}
+            int holder=vet[i];
+            menor=j;
+            vet[i]=vet[menor];
+            vet[j]=holder;
+        }
+    }
+}
+//quadratica
+printf("%d",vet[4]);
+}
