34f02da19e2d13a107beb83bb8bf2293880368c0
Fabio Schitini Alves de Oliveira <fabioschitini1@hotmail.com>
2022-09-13 20:21:10 -0300

    Update alunonaEscola.c

--- alunonaEscola.c
+++ alunonaEscola.c
@@ -1,69 +1,50 @@
 // Online C compiler to run C program online
+
 #include <stdio.h>
 
 int main() {
-    
-    
-    
-    
+    int opc;
+    printf("Selecione a opçao [1 = ALUNOS]: ");
+    scanf("%i", &opc);
+    switch (opc){
+        case 1:
+            aluno();
+        break;
+        default:
+            printf("Opçao fodase\n");
+            main();
+        break;
+    }
+}
+
+void aluno(){
+        
  typedef struct alunos{
     int matricula;
     int existe;
     int id;
 } Aluno;
-
-Aluno alunos[999];
-int contador=0;
-for(int i=0;i<3;i++){
-    printf("Colocque a matricula do corno: ");
-    scanf("%d",&alunos[i].matricula);
-    alunos[i].id=i;
-    alunos[i].existe=1;
+contador=0;
+    Aluno alunos[999];
+ cadrastar(){
+      printf("Colocque a matricula do corno: ");
+    scanf("%d",&alunos[contador].matricula);
+    alunos[contador].id=contador;
+    alunos[contador].existe=1;
     contador++;
-}
-
-for(int n=0;n<contador;n++){
-        printf("%d \n",alunos[n].matricula);
-}
-
-  int idDeletar;
-printf("Coloque o id do corno que voce quer deletar: ");
-scanf("%d",&idDeletar);
-
-//printf("idDeletar: %d \n",idDeletar);
-if(idDeletar<contador){
-        alunos[idDeletar].existe=0;
-} 
-else{
-    printf("Id nao existe foollll\n");
-}
-
-for(int m=0;m<contador;m++){
-    if(alunos[m].existe){
-        printf("%d\n",alunos[m].matricula);
+ }
+    int opc;
+    printf("Selecione a opçao [1 = CADASTRAR, 2 = SAIR]: ");
+    scanf("%i", &opc);
+    switch (opc){
+        case 1:
+            //cadastrar();
+        break;
+        case 2:
+            main();
+        break;
+        default:
+            printf("Opçao fodase");
+        break;
     }
-}
-
-int opc, idopc;
-printf("Selecione o ID do aluno a dar update: ");
-scanf("%i", &idopc);
-
-printf("Selecione a opção [1 = Matricula]: ");
-scanf("%i", &opc);
-
-switch(opc){
-    case 1:
-        printf("Coloque a nova matricula do corno: ");
-        scanf("%d",&alunos[idopc].matricula);
-    break;
-    
-    default:
-        printf("Opção inválida.");
-    break;
-}
-for(int x=0;x<contador;x++){
- if(alunos[x].existe){
-        printf("%d\n",alunos[x].matricula);
-    }
-}
 }
